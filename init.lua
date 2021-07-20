b48e1a0ef110dd27ebb8cdb5af8664731e5cab74
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-07-20 15:13:16 +0800

    Feat: add HTML, CSS, and Powershell language servers

--- init.lua
+++ init.lua
@@ -92,10 +92,13 @@ vim.g.lightline = {
 vim.g.coc_global_extensions = {
     'coc-clangd',
     'coc-cmake',
+    'coc-css',
     'coc-deno',
     'coc-emmet',
+    'coc-html',
     'coc-json',
     'coc-pairs',
+    'coc-powershell',
     'coc-pyright',
     'coc-rust-analyzer',
     'coc-tsserver',
