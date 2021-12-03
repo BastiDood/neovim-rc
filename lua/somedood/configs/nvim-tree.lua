b07d802187d91014e9d81ab77725b33423d33493
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-12-03 20:01:14 +0800

    Fix: remove unused option for `nvim-tree`

--- lua/somedood/configs/nvim-tree.lua
+++ lua/somedood/configs/nvim-tree.lua
@@ -1,6 +1,5 @@
 return function()
     require'nvim-tree'.setup {
-        ignore_ft_on_setup = { '.vim', '.git', '.github', '.vscode', 'node_modules', 'target', 'build' },
         update_cwd = true,
         hijack_cursor = true,
         filters = { dotfiles = true },
