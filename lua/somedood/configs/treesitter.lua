fd8a5048814fcbd3abf6ce04d75d140ce4f861e5
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-02-14 14:17:00 +0800

    Feat: ensure that Tree-sitter languages are installed

--- lua/somedood/configs/treesitter.lua
+++ lua/somedood/configs/treesitter.lua
@@ -1,5 +1,26 @@
 return function()
     require'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+            'c',
+            'cmake',
+            'comment',
+            'cpp',
+            'css',
+            'html',
+            'javascript',
+            'jsdoc',
+            'jsonc',
+            'latex',
+            'lua',
+            'markdown',
+            'python',
+            'rust',
+            'toml',
+            'tsx',
+            'typescript',
+            'verilog',
+            'yaml',
+        },
         indent = { enable = true },
         highlight = {
             enable = true,
