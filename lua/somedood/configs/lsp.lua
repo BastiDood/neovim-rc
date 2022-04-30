673ee825a9c492ecd4d67c5d7242a0cf087c4821
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-05-01 01:46:45 +0800

    Fix: remove Verible support

--- lua/somedood/configs/lsp.lua
+++ lua/somedood/configs/lsp.lua
@@ -56,8 +56,7 @@ return function()
     lsp.cmake.setup { on_attach = on_lsp_attach, capabilities = caps }
     lsp.emmet_ls.setup { on_attach = on_lsp_attach, capabilities = caps }
     lsp.html.setup { on_attach = on_lsp_attach, capabilities = caps }
-    lsp.svls.setup { on_attach = on_lsp_attach, capabilities = caps }
-    lsp.verible.setup { on_attach = on_lsp_attach, capabilities = caps }
+    lsp.svls.setup { on_attach = on_lsp_attach, capabilities = caps, single_file_support = true }
 
     -- Advanced Clangd Configuration
     lsp.clangd.setup {
