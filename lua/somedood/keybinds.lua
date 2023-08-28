30438e568cf70c9d5528994724d12fbeb308051b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-28 17:20:33 +0800

    feat: add font resize shortcuts

--- lua/somedood/keybinds.lua
+++ lua/somedood/keybinds.lua
@@ -1,11 +1,43 @@
 local noremap = { noremap = true }
-local silent_noremap = { noremap = true, silent = true }
+
+-- Font Resizing
+local default = 10
+local size = default
+
+local function update_font()
+    vim.opt.guifont = 'JetBrains Mono:h' .. size
+end
+
+local function update_with_print()
+    update_font()
+    vim.print('Font size changed to ' .. size)
+end
+
+local function rst_font()
+    size = default
+    update_with_print()
+end
+
+local function inc_font()
+    size = size + 1
+    update_with_print()
+end
+
+local function dec_font()
+    size = math.max(size - 1, 2)
+    update_with_print()
+end
+
+update_font()
+vim.keymap.set('n', '<C-0>', rst_font, noremap)
+vim.keymap.set('n', '<C-->', dec_font, noremap)
+vim.keymap.set('n', '<C-=>', inc_font, noremap)
 
 -- Lazy.nvim
-vim.keymap.set('n', '<leader>l', function() require'lazy'.home() end, silent_noremap)
+vim.keymap.set('n', '<leader>l', function() require'lazy'.home() end, noremap)
 
 -- Neovim
-vim.keymap.set('n', '<C-s>', [[<cmd>up<cr>]], silent_noremap)
+vim.keymap.set('n', '<C-s>', [[<cmd>up<cr>]], noremap)
 vim.keymap.set('n', '<C-S-k>', [[<cmd>m .-2<cr>==]], noremap)
 vim.keymap.set('n', '<C-S-j>', [[<cmd>m .+1<cr>==]], noremap)
 vim.keymap.set('x', '<leader>p', '"_dP', noremap)
