0de031794951aaeaae37262fa8a24362455f4a1f
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-27 16:16:25 +0800

    fix(lsp): only check schemas for a few configs

--- lua/somedood/configs/lsp.lua
+++ lua/somedood/configs/lsp.lua
@@ -147,7 +147,16 @@ return function()
             settings = {
                 json = {
                     validate = { enable = true },
-                    schemas = require'schemastore'.json.schemas(),
+                    schemas = require'schemastore'.json.schemas {
+                        select = {
+                            '.eslintrc',
+                            '.htmlhintrc',
+                            'Deno Config (deno.json)',
+                            'package.json',
+                            'prettierrc.json',
+                            'Stylelint (.stylelintrc)',
+                        },
+                    },
                 },
             },
         },
