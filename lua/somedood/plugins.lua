fbdf80217948d8134e049ef0b6ce454ed64f5cbc
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-28 03:59:54 +0800

    refactor: flatten all dependency arrays

--- lua/somedood/plugins.lua
+++ lua/somedood/plugins.lua
@@ -18,6 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Define plugins.
 require'lazy'.setup {
+    defaults = { lazy = true },
     spec = {
         {
             'sainnhe/edge',
@@ -27,30 +28,29 @@ require'lazy'.setup {
         },
         {
             'echasnovski/mini.statusline',
+            lazy = false,
             version = false,
-            opts = {
-                use_icons = false,
-                set_vim_settings = false,
-            },
+            opts = { use_icons = false, set_vim_settings = false },
+        },
+        {
+            'lukas-reineke/indent-blankline.nvim',
+            event = { 'BufReadPost', 'BufNewFile' },
+            main = 'ibl', opts = { },
         },
         { 'echasnovski/mini.ai', version = false, event = 'InsertEnter', opts = { } },
         { 'kylechui/nvim-surround', version = '*', event = 'InsertEnter', opts = { } },
-        { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = { } },
         { 'tpope/vim-sleuth', event = { 'BufReadPost', 'BufNewFile' } },
         { 'tpope/vim-fugitive', keys = { { '<leader>Gs', function() vim.cmd 'Git' end } } },
+        { 'L3MON4D3/LuaSnip', dependencies = { 'rafamadriz/friendly-snippets' } },
+        {
+            'rafamadriz/friendly-snippets',
+            config = function() require'luasnip.loaders.from_vscode'.lazy_load() end,
+        },
         {
             'hrsh7th/nvim-cmp',
             event = 'InsertEnter',
             dependencies = {
-                {
-                    'L3MON4D3/LuaSnip',
-                    dependencies = {
-                        {
-                            'rafamadriz/friendly-snippets',
-                            config = function() require'luasnip.loaders.from_vscode'.lazy_load() end,
-                        },
-                    },
-                },
+                'L3MON4D3/LuaSnip',
                 'saadparwaiz1/cmp_luasnip',
                 'hrsh7th/cmp-buffer',
                 'hrsh7th/cmp-nvim-lsp',
@@ -64,20 +64,22 @@ require'lazy'.setup {
             opts = { check_ts = true },
         },
         {
+            'williamboman/mason.nvim',
+            opts = { },
+            keys = { { '<leader>m', function() vim.cmd 'Mason' end } },
+        },
+        { 'j-hui/fidget.nvim', opts = { } },
+        {
             'neovim/nvim-lspconfig',
             event = { 'BufReadPost', 'BufNewFile' },
             cmd = { 'LspInfo', 'LspInstall', 'LspUninstall' },
             dependencies = {
-                {
-                    'williamboman/mason.nvim',
-                    opts = { },
-                    keys = { { '<leader>m', function() vim.cmd 'Mason' end } },
-                },
+                'williamboman/mason.nvim',
                 'williamboman/mason-lspconfig.nvim',
                 'WhoIsSethDaniel/mason-tool-installer.nvim',
                 'hrsh7th/cmp-nvim-lsp',
                 'b0o/schemastore.nvim',
-                { 'j-hui/fidget.nvim', opts = { } },
+                'j-hui/fidget.nvim',
             },
             config = require'somedood.configs.lsp',
         },
@@ -108,39 +110,39 @@ require'lazy'.setup {
             },
         },
         {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'cmake -S. -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+            config = function() require'telescope'.load_extension'fzf' end,
+        },
+        {
+            'nvim-telescope/telescope-ui-select.nvim',
+            config = function() require'telescope'.load_extension'ui-select' end,
+        },
+        {
             'nvim-telescope/telescope.nvim',
             dependencies = {
                 'nvim-lua/popup.nvim',
                 'nvim-lua/plenary.nvim',
-                {
-                    'nvim-telescope/telescope-fzf-native.nvim',
-                    build = 'cmake -S. -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-                    config = function() require'telescope'.load_extension'fzf' end,
-                },
-                {
-                    'nvim-telescope/telescope-ui-select.nvim',
-                    config = function() require'telescope'.load_extension'ui-select' end,
-                },
+                'nvim-telescope/telescope-fzf-native.nvim',
+                'nvim-telescope/telescope-ui-select.nvim',
             },
             keys = {
-                { '<C-f>' , function() require'telescope.builtin'.current_buffer_fuzzy_find() end },
-                { '<C-S-f>' , function() require'telescope.builtin'.live_grep() end },
-                { '<C-,>' , function() require'telescope.builtin'.find_files { cwd = vim.fn.stdpath 'config' } end },
-                { '<leader>ff' , function() require'telescope.builtin'.find_files() end },
-                { '<leader>ft' , function() require'telescope.builtin'.treesitter() end },
-                { '<leader>fG' , function() require'telescope.builtin'.git_files() end },
-                { '<leader>Gb' , function() require'telescope.builtin'.git_branches() end },
-                { '<leader>fb' , function() require'telescope.builtin'.buffers() end },
+                { '<C-f>', function() require'telescope.builtin'.current_buffer_fuzzy_find() end },
+                { '<C-S-f>', function() require'telescope.builtin'.live_grep() end },
+                { '<C-,>', function() require'telescope.builtin'.find_files { cwd = vim.fn.stdpath 'config' } end },
+                { '<leader>ff', function() require'telescope.builtin'.find_files() end },
+                { '<leader>ft', function() require'telescope.builtin'.treesitter() end },
+                { '<leader>fG', function() require'telescope.builtin'.git_files() end },
+                { '<leader>Gb', function() require'telescope.builtin'.git_branches() end },
+                { '<leader>fb', function() require'telescope.builtin'.buffers() end },
             },
             config = require'somedood.configs.telescope',
         },
+        { 'nvim-treesitter/nvim-treesitter-context', opts = { } },
         {
             'nvim-treesitter/nvim-treesitter',
             build = ':TSUpdate',
-            dependencies = {
-                { 'nvim-treesitter/nvim-treesitter-context', opts = { } },
-                'nushell/tree-sitter-nu',
-            },
+            dependencies = { 'nvim-treesitter/nvim-treesitter-context', 'nushell/tree-sitter-nu' },
             cmd = { 'TSUpdate', 'TSInstall', 'TSUninstall' },
             event = { 'BufReadPost', 'BufNewFile' },
             keys = { { 'c[', function() require'treesitter-context'.go_to_context() end } },
@@ -174,10 +176,7 @@ require'lazy'.setup {
                     'zig',
                 },
                 indent = { enable = true },
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
+                highlight = { enable = true, additional_vim_regex_highlighting = false },
             },
             config = function(_, opts)
                 -- Prefer Git instead of cURL in order to improve connectivity in some environments.
