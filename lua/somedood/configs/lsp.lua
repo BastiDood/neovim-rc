5689f7a132383de26270040d6da6ab60e3cc6850
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-09-17 23:10:29 +0800

    feat(lsp): only highlight references on keybind

--- lua/somedood/configs/lsp.lua
+++ lua/somedood/configs/lsp.lua
@@ -36,13 +36,9 @@ return function()
             local client = vim.lsp.get_client_by_id(event.data.client_id)
             if client then
                 if client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-                    -- Highlight References under Cursor
+                    -- Highlight References at Cursor
+                    map('<leader>hr', vim.lsp.buf.document_highlight, 'Highlight References at Cursor')
                     local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-                    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
                     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
                         buffer = event.buf,
                         group = highlight_augroup,
