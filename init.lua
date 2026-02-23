04a1ef068ed4b37143a45f9711a1fa3542beba36
Basti Ortiz <ortiz@bastidood.dev>
2026-02-23 19:08:42 +0800

    fix: add default indentation widths

--- init.lua
+++ init.lua
@@ -37,6 +37,9 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- Indentation Controls
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 
